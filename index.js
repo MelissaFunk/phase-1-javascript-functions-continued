694d296f3d6330762a0e26e93850066f02248a88
+ <+>
2021-08-10 15:48:10 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun(fun='roller-skate') {
+    return `This Saturday, I want to ${fun}!`
+    }
+
+const mondayWork = function(monday='go to the office') {
+    return `This Monday, I will ${monday}.`
+
+}
+
+const wrapAdjective = function(flair='*'){
+    return function(adjective='special') {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+} 
